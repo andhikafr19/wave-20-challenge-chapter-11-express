14b5a53166cb1bbcae92ca8050df07642c70a863
Andhika Ramadhan <andhikafirdaus@gmail.com>
2022-09-05 18:47:46 +0700

    change env-2

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const playerRoute = require('./routes/player');
 const authRoute = require('./routes/auth');
 
+const port = process.env.PORT || 3000;
+
 mongoose.connect('mongodb+srv://shaniasad:secre7@cluster0.vp4fu.mongodb.net/?retryWrites=true&w=majority')
 
 const app = express()
@@ -30,6 +32,6 @@ app.use((req, res) => {
     res.status(404).send("<h1> Endpoint not found.</h1>")
 })
 
-app.listen(process.env.PORT||8000, () => {
-    console.log('server listening on http://localhost:8000.')
+app.listen(port, () => {
+    console.log(`server listening on ${port}`)
 })
